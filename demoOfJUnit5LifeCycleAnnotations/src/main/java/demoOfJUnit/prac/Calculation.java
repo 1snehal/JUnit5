8c606616423dfaeff10e834accc19a4d225a36ba
1snehal <78212185+1snehal@users.noreply.github.com>
2022-05-17 13:53:51 +0530

    Add files via upload

---
+++ demoOfJUnit5LifeCycleAnnotations/src/main/java/demoOfJUnit/prac/Calculation.java
@@ -0,0 +1,17 @@
+package demoOfJUnit.prac;
+public class Calculation {
+	public int sum(int a,int b){
+		return a+b;
+	}
+	public double circlearea(double radius)
+	{
+		return Math.PI*radius*radius;
+	}
+	public int dividenumber(int a, int b) {
+		return a/b;
+	}
+	public double multiplynumbers(double a,double b)
+	{
+		return  a*b;
+	}
+}
