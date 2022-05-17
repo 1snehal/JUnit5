dadfb384af52fd696d0a99fff48220a315271900
1snehal <78212185+1snehal@users.noreply.github.com>
2022-05-17 13:57:49 +0530

    Add files via upload

---
+++ demoOfJUnitLifeCycleNestedassertmessage/src/test/java/com/junit/prac/CalculationTest.java
@@ -0,0 +1,48 @@
+package com.junit.prac;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+class CalculationTest {
+	Calculation calculation;
+	@BeforeEach
+	void init(){
+		calculation=new Calculation();
+	}
+	@Nested
+	class CalculationForAddTest{
+		@Test
+		@DisplayName("Testing add method for positive number")
+		void testforadditionpositive() {
+			assertEquals(4, calculation.sum(1, 3),"The sum method performing the addition of two numbers");
+		}
+		@Test
+		@DisplayName("Testing add method for negative number")  
+		void testforadditionnegative() {                           //this method execute only when the test case is failed
+			int expected=-8;
+			int actual=calculation.sum(-2, -6);
+			assertEquals(expected,actual,()->"shuld retured sum"+expected+"but retured"+actual);
+		}
+	}
+	@Test
+	void testforcirclerarea(){
+		assertEquals(314.1592653589793,calculation.circlearea(10),"The Circlearea method performing area of circle");
+	}
+	@Test
+	void testformultiplication(){
+		assertEquals(8, calculation.multiplynumbers(2, 4),"The multiplynumbers method performing multiplication of two numbers");
+	}
+	@Test
+	void testfordivision() {
+		assertThrows(ArithmeticException.class,()->calculation.dividenumber(6,0),"Divide by zero");
+	}
+	@Test
+	@Disabled
+	@DisplayName("method disabled")
+	void testfailed()
+	{
+		fail("this method is disabled");
+	}
+}
